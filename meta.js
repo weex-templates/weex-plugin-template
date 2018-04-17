8978454c409c7020a53a2809ba85c44eacd31ebc
erha19 <faterrole@gmail.com>
2018-04-17 16:56:50 +0800

    Update meta.js

--- meta.js
+++ meta.js
@@ -36,35 +36,33 @@ module.exports = {
       when: 'isNotTest',
       type: 'string',
       required: true,
-      message: 'please confirm Android Project Name',
-      default: iOSProjectName
+      message: 'please confirm Android Project Name'
     },
     AndroidProjectName: {
       when: 'isNotTest',
       type: 'string',
       required: true,
-      message: 'please confirm iOS Project Name',
-      default: AndroidProjectName
+      message: 'please confirm iOS Project Name'
     }
   },
   filters: {},
   complete: function(data, { chalk }) {
-    const green = chalk.green
+    // const green = chalk.green
 
-    sortDependencies(data, green)
+    // sortDependencies(data, green)
 
-    const cwd = data.dest ? data.dest : path.join(process.cwd(), data.inPlace ? '' : data.destDirName)
+    // const cwd = data.dest ? data.dest : path.join(process.cwd(), data.inPlace ? '' : data.destDirName)
 
-    if (data.autoInstall) {
-      installDependencies(cwd, data.autoInstall, green)
-        .then(() => {
-          // printMessage(data, green)
-        })
-        .catch(e => {
-          console.log(chalk.red('Error:'), e)
-        })
-    } else {
-      // printMessage(data, chalk)
-    }
+    // if (data.autoInstall) {
+    //   installDependencies(cwd, data.autoInstall, green)
+    //     .then(() => {
+    //       // printMessage(data, green)
+    //     })
+    //     .catch(e => {
+    //       console.log(chalk.red('Error:'), e)
+    //     })
+    // } else {
+    //   // printMessage(data, chalk)
+    // }
   },
 }
