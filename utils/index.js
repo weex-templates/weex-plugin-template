908c23caca975ba921a91a0778e29cf28314f324
erha19 <faterrole@gmail.com>
2018-04-17 21:17:56 +0800

    Update message after render the template

--- utils/index.js
+++ utils/index.js
@@ -72,52 +72,6 @@ exports.printMessage = function printMessage(data, { green, yellow }) {
         'Starts the development server for you to preview your weex page on browser',
         'You can also scan the QR code using weex playground to preview weex page on native'
       ]
-    },
-    {
-      name: 'npm run dev',
-      desc: [
-        'Open the code compilation task in watch mode'
-      ]
-    },
-    {
-      name: 'npm run ios',
-      desc: [
-        '(Mac only, requires Xcode)',
-        'Starts the development server and loads your app in an iOS simulator'
-      ]
-    },
-    {
-      name: 'npm run android',
-      desc: [
-        '(Requires Android build tools)',
-        'Starts the development server and loads your app on a connected Android device or emulator'
-      ]
-    },
-    {
-      name: 'npm run pack:ios',
-      desc: [
-        '(Mac only, requires Xcode)',
-        'Packaging ios project into ipa package'
-      ]
-    },
-    {
-      name: 'npm run pack:android',
-      desc: [
-        '(Requires Android build tools)',
-        'Packaging android project into apk package'
-      ]
-    },
-    {
-      name: 'npm run pack:web',
-      desc: [
-        'Packaging html5 project into `web/build` folder'
-      ]
-    },
-    {
-      name: 'npm run test',
-      desc: [
-        'Starts the test runner'
-      ]
     }
   ];
   console.log(`\n${green(`Success! Created ${data.destDirName} at ${path.resolve(data.destDirName)}`)}`)
