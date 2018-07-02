050511800ce800f7717d3aa35bad09e639ec6f4e
Tiago Alves <tralves@gmail.com>
2018-07-02 13:26:28 +0100

    Fixed android plugin example.

--- template/android/library/src/main/java/org/weex/plugin/{{AndroidProjectName}}/{{upperCamelCaseName}}Module.java
+++ template/android/library/src/main/java/org/weex/plugin/{{AndroidProjectName}}/{{upperCamelCaseName}}Module.java
@@ -1,5 +1,8 @@
 package org.weex.plugin.{{AndroidProjectName}};
 
+import android.util.Log;
+import android.widget.Toast;
+
 import com.alibaba.weex.plugin.annotation.WeexModule;
 import com.taobao.weex.annotation.JSMethod;
 import com.taobao.weex.bridge.JSCallback;
@@ -7,18 +10,26 @@ import com.taobao.weex.common.WXModule;
 
 @WeexModule(name = "{{lowerCamelCaseName}}")
 public class {{upperCamelCaseName}}Module extends WXModule {
-    
+
+    private static final String TAG = "{{upperCamelCaseName}}Module";
+
     //sync ret example
     //TODO: Auto-generated method example
-    @JSMethod
+    @JSMethod (uiThread = true)
     public String syncRet(String param) {
         return param;
     }
 
     //async ret example
     //TODO: Auto-generated method example
-    @JSMethod
+    @JSMethod (uiThread = true)
     public void asyncRet(String param, JSCallback callback) {
         callback.invoke(param);
+    }
+
+    @JSMethod (uiThread = true)
+    public void show() {
+        Log.d(TAG, "Showing!!!");
+        Toast.makeText(mWXSDKInstance.getContext(),"Module {{lowerCamelCaseName}} is created sucessfully ",Toast.LENGTH_SHORT).show();
     }
 }
